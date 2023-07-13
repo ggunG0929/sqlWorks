b743ddac3b82260ca8fd22079972ec4409e8572c
ggunG <ggung0929@gmail.com>
2023-07-13 18:01:35 +0900

    7.13 주석 추가

--- mysqlWorks/member_board.sql
+++ mysqlWorks/member_board.sql
@@ -3,27 +3,45 @@ use mydb;
 
 -- 회원 테이블
 create table t_member(
-	memberid	varchar(20),
-    passwd		varchar(20) not null,
-    name		varchar(30) not null,
-    gender		varchar(10),
-    joindate	timestamp default now(),
+	memberid	varchar(20),				-- 회원 아이디(기본키)
+    passwd		varchar(20) not null,		-- 비밀번호
+    name		varchar(30) not null,		-- 이름
+    gender		varchar(10),				-- 성별
+    joindate	timestamp default now(),	-- 가입일
     primary key(memberid)
 );
 
 -- 게시판 테이블
 create table t_board(
-	bnum	int primary key auto_increment,		-- 자동순번
-    title	varchar(100) not null,
-    content 	text,
-    regdate 	timestamp default now(),
-    modifydate	timestamp,
-    hit		int default 0,
-    memberid	varchar(20),
-    fileupload	varchar(50),
-    constraint fk_member_board foreign key(memberid)
+	bnum	int primary key auto_increment,		-- 게시글 번호(기본키)	-- 자동순번
+    title	varchar(100) not null,				-- 글 제목
+    content 	text,							-- 글 내용
+    regdate 	timestamp default now(),		-- 작성일
+    modifydate	timestamp,						-- 수정일
+    hit		int default 0,						-- 조회수
+    memberid	varchar(20),					-- 작성자(외래키)
+    fileupload	varchar(50),					-- 첨부파일
+    constraint fk_member_board foreign key(memberid)	-- 외래키 제약조건(회원 탈퇴시 게시글도 삭제됨)
     references t_member(memberid) on delete cascade
 );
+
+-- 댓글 테이블
+create table t_reply(
+	rno			int primary key auto_increment,		-- 댓글번호(기본키) 자동순번
+    bnum		int not null,						-- 게시글 번호(외래키)
+    rcontent	text not null,						-- 댓글 내용
+    replyer		varchar(30) not null,				-- 작성자
+    rdate		timestamp default now(),			-- 작성일
+    rupdate		timestamp,							-- 수정일
+	constraint fk_reply_board foreign key(bnum)		-- 외래키 제약조건(게시글 삭제시 댓글도 삭제됨)
+    references t_board(bnum) on delete cascade
+);
+
+-- 댓글 추가
+insert into t_reply(bnum, rcontent, replyer) values(1, '내용에 대한 댓글입니다.', 'sunny');
+
+-- 댓글 검색
+select * from t_reply;
 
 -- 게시글 쓰기
 insert into t_board(title, content, memberid) values('가입인사', '안녕하세요. 가입했습니다.', 'today');
