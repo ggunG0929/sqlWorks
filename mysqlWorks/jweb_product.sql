43b0342dab456a52ee52ec7a2f1b4d300b289c0b
ggunG <ggung0929@gmail.com>
2023-07-20 18:25:52 +0900

    7.20 market

--- mysqlWorks/jweb_product.sql
+++ mysqlWorks/jweb_product.sql
@@ -15,3 +15,7 @@ CREATE TABLE product(
 
 -- 상품 추가
 INSERT INTO product VALUES ('P1234', 'iPhone 6s', 1200000, '1334X750 Renia HD Display', 'Smart Phone', 'Apple', 1000, 'New', 'P1234.png');
+
+select * from product;
+
+select * from product where p_id = 'P1234';
